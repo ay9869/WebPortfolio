cff96656d7232dc1e2b94854bb548e285325b15a
Abhimanyu Yadav <67111688+ay9869@users.noreply.github.com>
2025-01-01 19:17:54 -0600

    Add files via upload

---
+++ website_html5/Backupindex.py
@@ -0,0 +1,176 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Divyanshu Mishra's Portfolio</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+</head>
+<body class="is-preload">
+
+    <!-- Wrapper -->
+    <div id="wrapper" class="fade-in">
+
+        <!-- Intro -->
+        <div id="intro">
+            <h1>Data Science<br />Portfolio</h1>
+            <!-- Add your photo here -->
+            <img src="assets/images/myphoto.jpg" alt="Divyanshu Mishra" style="width:250px; border-radius:15px; margin-top:20px; border: 2px solid #ccc;" />
+            <p>Follow me on LinkedIn <a href="https://www.linkedin.com/in/divmishra97/">@divmishra97</a></p>
+            <ul class="actions">
+                <li><a href="#header" class="button icon solid solo fa-arrow-down scrolly">Continue</a></li>
+            </ul>
+        </div>
+
+        <!-- Header -->
+        <header id="header">
+            <a href="index.html" class="logo">Portfolio</a>
+        </header>
+
+        <!-- Nav -->
+        <nav id="nav">
+            <ul class="links">
+                <li class="active"><a href="index.html">Portfolio</a></li>
+                <li><a href="resume.html">Resume</a></li>
+                <li><a href="about me.html">About Me</a></li>
+            </ul>
+            <ul class="icons">
+                <li><a href="https://github.com/DivyanshuMishra97" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
+                <li><a href="https://www.linkedin.com/in/divmishra97/" class="icon brands fa-linkedin"><span class="label">LinkedIn</span></a></li>
+            </ul>
+        </nav>
+
+        <!-- Main -->
+        <div id="main">
+            <!-- Featured Post -->
+            <article class="post featured">
+                <header class="major">
+                    <span class="date">December 23, 2024</span>
+                    <h2><a href="https://github.com/DivyanshuMishra97/COVID-19-Global-Vaccine-Tracker">(Tableau) COVID-19 Global Vaccine Tracker: <br />A Data-Driven Perspective</a></h2>
+                    <p>Explore how data visualization transforms complex datasets into actionable insights with the <strong>COVID-19 Global Vaccine Tracker Dashboard</strong>. 
+                       This project leverages data from Kaggle to showcase global vaccination trends, regional disparities, and the relationship between GDP per capita and vaccination rates.</p>
+                    <p><strong>Key features of the dashboard include:</strong></p>
+                    <ul>
+                        <li><strong>Interactive Visualizations:</strong> A global map, bar charts, and scatter plots for in-depth analysis.</li>
+                        <li><strong>Dynamic Filters:</strong> Customize views by region, country, or time period.</li>
+                        <li><strong>Actionable Insights:</strong> Identify underperforming regions, track vaccination progress, and explore economic impacts.</li>
+                    </ul>
+                    <p>
+                        With this dashboard, administration bodies can uncover hidden patterns, prioritize resources, and strategize for equitable vaccine distribution. Built using Tableau with a robust ETL process in Python and Excel, this project demonstrates the power of data to drive impactful decisions.
+                    
+                    </p>
+            </header>
+            <a href="https://github.com/DivyanshuMishra97/COVID-19-Global-Vaccine-Tracker" class="image main"><img src="images/1.jpg" alt="COVID-19 Global Vaccine Tracker" /></a>
+            <ul class="actions special">
+                <li><a href="https://github.com/DivyanshuMishra97/COVID-19-Global-Vaccine-Tracker" class="button large">Read More</a></li>
+            </ul>
+            </article>
+
+            <!-- Posts -->
+            <section class="posts">
+                <article>
+                    <header>
+                        <span class="date">December 14, 2024</span>
+                        <h2><a href="https://github.com/DivyanshuMishra97/Sales_Insights">(Power BI) Unlocking Business Potential:<br />Sales Insights and Analysis</a></h2>
+                    </header>
+                    <a href="https://github.com/DivyanshuMishra97/Sales_Insights" class="image main"><img src="images/2.jpg" alt="Sales Insights" /></a>
+                    <p>This project leverages data analytics to extract actionable insights from sales data, helping businesses make informed decisions. Key highlights include analyzing sales trends, customer behavior, and visualizing performance metrics. Using tools like Power BI, MySQL, Excel, and Python, the project delivers reports and models to optimize strategies and improve revenue.</p>
+                    <ul class="actions special">
+                        <li><a href="https://github.com/DivyanshuMishra97/Sales_Insights" class="button">Read More</a></li>
+                    </ul>
+                </article>
+                <article>
+                    <header>
+                        <span class="date">December 10, 2024</span>
+                        <h2><a href="https://github.com/DivyanshuMishra97/Applestore_Insights_SQL">(SQL)Applestore Insights:<br />Data-Driven Store Performance Analysis</a></h2>
+                    </header>
+                    <a href="https://github.com/DivyanshuMishra97/Applestore_Insights_SQL" class="image fit"><img src="images/3.jpg" alt="Applestore Insights" /></a>
+                    <p>The Applestore Insights SQL project uses SQL to analyze Apple Store data, focusing on customer behavior, sales trends, and inventory. It identifies top-selling products, regional sales, and purchase patterns using techniques like CASE, JOINs, and GROUP BY to generate actionable insights, including revenue trends and growth opportunities.</p>
+                    <ul class="actions special">
+                        <li><a href="https://github.com/DivyanshuMishra97/Applestore_Insights_SQL" class="button">Read More</a></li>
+                    </ul>
+                </article>
+                <article>
+                    <header>
+                        <span class="date">December 5, 2024</span>
+                        <h2><a href="https://github.com/DivyanshuMishra97/Personal_Finance_dashboard">(Python) Personal Finance Dashboard:<br />Simplify Your Financial Management</a></h2>
+                    </header>
+                    <a href="https://github.com/DivyanshuMishra97/Personal_Finance_dashboard" class="image fit"><img src="images/4.jpg" alt="Personal Finance Dashboard" /></a>
+                    <p>The Personal Finance Dashboard is a Python-based tool for analyzing and visualizing financial transactions. It categorizes spending into groups like Groceries, Travel, and Restaurants. Built with Panel and hvPlot, it features interactive charts and real-time savings calculations to provide actionable insights, including monthly expenses, category-wise breakdowns, and financial health analysis.</p>
+                    <ul class="actions special">
+                        <li><a href="https://github.com/DivyanshuMishra97/Personal_Finance_dashboard" class="button">Read More</a></li>
+                    </ul>
+                </article>
+                <article>
+                    <header>
+                        <span class="date">December 1, 2024</span>
+                        <h2><a href="https://github.com/DivyanshuMishra97/Youtube_Channel_Insights">(Tableau) YouTube Channel Insights:<br />Optimize Content Strategy with Data</a></h2>
+                    </header>
+                    <a href="https://github.com/DivyanshuMishra97/Youtube_Channel_Insights" class="image fit"><img src="images/5.jpg" alt="Insights Dashboard" /></a>
+                    <p>The YouTube Channel Insights project leverages data analysis to evaluate key performance metrics, such as video views, engagement rates, and subscriber growth. Using Tableau and the YouTube Data API, it provides actionable insights to optimize content strategy, enhance audience engagement, and track growth trends through visualizations like engagement distribution, subscriber growth charts, and audience retention analysis.</p>
+                    <ul class="actions special">
+                        <li><a href="https://github.com/DivyanshuMishra97/Youtube_Channel_Insights" class="button">Read More</a></li>
+                    </ul>
+                </article>
+            </section>
+
+            <!-- Footer -->
+            <footer>
+                <div class="pagination">
+                    <a href="#" class="page active">1</a>
+                    <a href="#" class="page">2</a>
+                    <a href="#" class="page">3</a>
+                    <span class="extra">&hellip;</span>
+                    <a href="#" class="page">8</a>
+                    <a href="#" class="page">9</a>
+                    <a href="#" class="page">10</a>
+                    <a href="#" class="next">Next</a>
+                </div>
+            </footer>
+
+        </div>
+
+        <!-- Footer -->
+        <footer id="footer">
+            <section class="split contact">
+                <section class="alt">
+                    <h3>Address</h3>
+                    <p>7575 Frankford Road<br />Dallas, TX 75080, USA</p>
+                </section>
+                <section>
+                    <h3>Phone</h3>
+                    <p><a href="tel:+14695695899">+1 (469) 569-5899</a></p>
+                </section>
+                <section>
+                    <h3>Email</h3>
+                    <p><a href="mailto:divyanshu.mishra@utdallas.edu">divyanshu.mishra@utdallas.edu</a></p>
+                </section>
+                <section>
+                    <h3>Social</h3>
+                    <ul class="icons alt">
+                        <li><a href="https://github.com/DivyanshuMishra97" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
+                        <li><a href="https://www.linkedin.com/in/divmishra97/" class="icon brands alt fa-linkedin"><span class="label">LinkedIn</span></a></li>
+                    </ul>
+                </section>
+            </section>
+        </footer>
+
+        <!-- Copyright -->
+        <div id="copyright">
+            <ul><li>&copy; 2024 Divyanshu Mishra</li></ul>
+        </div>
+
+    </div>
+
+    <!-- Scripts -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/jquery.scrollex.min.js"></script>
+    <script src="assets/js/jquery.scrolly.min.js"></script>
+    <script src="assets/js/browser.min.js"></script>
+    <script src="assets/js/breakpoints.min.js"></script>
+    <script src="assets/js/util.js"></script>
+    <script src="assets/js/main.js"></script>
+
+</body>
+</html>
